b1d9f0b038b29d172b59d2e7c66820d42fc2982b
Crisktina <crishidalgolopez@gmail.com>
2022-11-15 08:53:14 +0100

    sounds linked with dev

--- script.js
+++ script.js
@@ -8,8 +8,22 @@ const tom2Play = document.getElementById("soundTom2");
 const tom1Play = document.getElementById("soundTom1");
 
 function playSound(drumSound) {
-    if (drumSound === "tom1") {
+    if (drumSound === "pad1") {
         // reproducir audio especifico de tom1
         tom1Play.play();
+    } else if (drumSound === "pad2") {
+        tom2Play.play();
+    } else if (drumSound === "pad3") {
+        tom3Play.play();
+    } else if (drumSound === "pad4") {
+        tom4Play.play();
+    } else if (drumSound === "padbombo") {
+        bomboPlay.play();
+    } else if (drumSound === "padCrash") {
+        crashPlay.play();
+    } else if (drumSound === "padHitHat") {
+        hitHatPlay.play();
+    } else if (drumSound === "padRedoblante") {
+        redoblantePlay.play();
     }
 }
